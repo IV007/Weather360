3787dcdc1fcb5edb07a396ebcdf15d63a435fe0f
Ivan Utsalo <ivan.utsalo.contractor@bbva.com>
2015-07-15 12:20:12 -0500

    Further work on Utilities

---
+++ app/src/main/java/com/sidelance/weather/weather360/AppConstants.java
@@ -0,0 +1,9 @@
+package com.sidelance.weather.weather360;
+
+/**
+ * Created by xsc2144 on 7/15/2015.
+ */
+public class AppConstants {
+
+    private static final String APP_SCRT = "67510f5340276195";
+}
--- app/src/main/java/com/sidelance/weather/weather360/Commons/tools/Tools.java
+++ app/src/main/java/com/sidelance/weather/weather360/Commons/tools/Tools.java
@@ -3,6 +3,9 @@ package com.sidelance.weather.weather360.Commons.tools;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  *Utility Methods
  */
@@ -26,4 +29,29 @@ public class Tools {
 
         return result;
     }
+
+    /**
+     * Gets the a digest from data
+     * @param data the input data
+     * @return the digest
+     */
+    public static byte[] getDigest(byte[] data) {
+        byte[] result = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA1");
+            digest.update(data);
+            result = digest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            Tools.logThrowable(TAG, e);
+            try {
+                MessageDigest digest = MessageDigest.getInstance("MD5");
+                digest.update(data);
+                result = digest.digest();
+            } catch (NoSuchAlgorithmException e2) {
+                Tools.logThrowable(TAG, e2);
+            }
+        }
+        return result;
+    }
+
 }
