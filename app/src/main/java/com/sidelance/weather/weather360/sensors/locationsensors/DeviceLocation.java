1f96f04989de41203a9d4ccdeb8c76325d2fb4bd
Ivan Utsalo <ivan.utsalo.contractor@bbva.com>
2015-07-17 19:05:49 -0500

    Initial work on GeoLocation.

--- app/src/main/java/com/sidelance/weather/weather360/sensors/locationsensors/DeviceLocation.java
+++ app/src/main/java/com/sidelance/weather/weather360/sensors/locationsensors/DeviceLocation.java
@@ -1,84 +1,142 @@
 package com.sidelance.weather.weather360.sensors.locationsensors;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.sidelance.weather.weather360.R;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Class that handles geolocation
  *
  */
-public class DeviceLocation extends Activity implements LocationListener {
+public class DeviceLocation extends Activity{
+
+    private static final String TAG = DeviceLocation.class.getSimpleName();
+
+    private LocationManager locationManager;
+
+    Geocoder geocoder;
+//    LocationListener listener;
+    String city, country, provider;
+    List<Address> addressList;
+    double x, y;
+    boolean clicked;
+
+    Button locationButton;
+    TextView userLocation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_user_location);
+        clicked = false;
+
+        locationButton =  ( Button )findViewById(R.id.locationButton);
+        userLocation = (TextView) findViewById(R.id.textView);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked = true;
+                locationButtonClicked();
+            }
+        });
     }
 
-    /**
-     * Called when the location has changed.
-     * <p/>
-     * <p> There are no restrictions on the use of the supplied Location object.
-     *
-     * @param location The new location, as a Location object.
-     */
-    @Override
-    public void onLocationChanged(Location location) {
 
+    private void locationButtonClicked(){
+
+        LocationListener listener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        Log.i(TAG, "==> Button CLick Event <==");
     }
 
-    /**
-     * Called when the provider status changes. This method is called when
-     * a provider is unable to fetch a location or if the provider has recently
-     * become available after a period of unavailability.
-     *
-     * @param provider the name of the location provider associated with this
-     *                 update.
-     * @param status   {@link LocationProvider#OUT_OF_SERVICE} if the
-     *                 provider is out of service, and this is not expected to change in the
-     *                 near future; {@link LocationProvider#TEMPORARILY_UNAVAILABLE} if
-     *                 the provider is temporarily unavailable but is expected to be available
-     *                 shortly; and {@link LocationProvider#AVAILABLE} if the
-     *                 provider is currently available.
-     * @param extras   an optional Bundle which will contain provider specific
-     *                 status variables.
-     *                 <p/>
-     *                 <p> A number of common key/value pairs for the extras Bundle are listed
-     *                 below. Providers that use any of the keys on this list must
-     *                 provide the corresponding value as described below.
-     *                 <p/>
-     *                 <ul>
-     *                 <li> satellites - the number of satellites used to derive the fix
-     */
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public class MyLocationListener implements LocationListener{
 
+        @SuppressWarnings("static-access")
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        public void onLocationChanged(Location location) {
+
+            x = location.getLatitude();
+            y = location.getLongitude();
+
+            try {
+                geocoder = new Geocoder(getApplicationContext(), Locale.ENGLISH);
+                addressList = geocoder.getFromLocation(x, y, 1);
+                StringBuilder stringBuilder = new StringBuilder();
+                if(geocoder.isPresent()){
+                    
+                    Log.e(TAG, "==> Geocoder Detected <==");
+
+                    Address returnAddress = addressList.get(0);
+
+                    city = returnAddress.getLocality();
+                    country = returnAddress.getCountryName();
+
+                    stringBuilder.append(city).append(", ");
+                    stringBuilder.append(country);
+
+
+                    userLocation.setText(stringBuilder);
+
+                }else{
+                    Log.e(TAG, "==> Geocoder Not Detected <==");
+                }
+
+            }catch(IOException e){
+                Log.e(TAG, "" + e.getMessage());
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
-    /**
-     * Called when the provider is enabled by the user.
-     *
-     * @param provider the name of the location provider associated with this
-     *                 update.
-     */
-    @Override
-    public void onProviderEnabled(String provider) {
-
+    public String getCity() {
+        return city;
     }
 
-    /**
-     * Called when the provider is disabled by the user. If requestLocationUpdates
-     * is called on an already disabled provider, this method is called
-     * immediately.
-     *
-     * @param provider the name of the location provider associated with this
-     *                 update.
-     */
-    @Override
-    public void onProviderDisabled(String provider) {
-
+    public String getCountry() {
+        return country;
     }
 }
